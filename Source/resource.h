bd9963410920d91bfe643293f92efd9199b3a177
ye546 <anton_bosse@hotmail.com>
2019-08-03 18:16:14 +0200

    resource

---
+++ Source/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by NoteApp.rc
+//
+#define IDR_MENU1                       101
+#define ID_FILE_OPEN                    40001
+#define ID_FILE_SAVE                    40003
+#define ID_FONT_ARIAL                   40027
+#define ID_FONT_TIMESNEWROMAN           40028
+#define ID_TIMESNEWROMAN_ITALIC         40029
+#define ID_TIMESNEWROMAN_NORMAL         40030
+#define ID_TIMESNEWROMAN_BOLD           40031
+#define ID_TIMESNEWROMAN_UNDERLINE      40032
+#define ID_ARIAL_ITALIC                 40033
+#define ID_ARIAL_NORMAL                 40034
+#define ID_ARIAL_BOLD                   40035
+#define ID_ARIAL_UNDERLINE              40036
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40037
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
